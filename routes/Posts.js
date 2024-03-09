bd6fa955d69d2357756a03e9b932126fc526fa67
kwagyei <kwabenasasuboakye@gmail.com>
2024-03-09 22:28:30 +0000

    remmove username field when creating a post

--- routes/Posts.js
+++ routes/Posts.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { Posts } = require("../models");
+const { validateToken } = require("../middlewares/AuthMiddleware")
 
 
 //API for displaying all posts
@@ -20,9 +21,11 @@ router.get("/:id", async(req, res) => {
 });
 
 //API for creating a POST
-router.post("/", async(req, res) => {
+router.post("/", validateToken, async(req, res) => {
 
     const post = req.body;
+    const postusername = req.user.username
+    post.userName = postusername
     await Posts.create(post); // this is also a seqeulize function
     res.json(post);
 
